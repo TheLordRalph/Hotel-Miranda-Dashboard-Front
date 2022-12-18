f7cfdfc923949f5a2f20bbbfce08316940ef130c
TheLordRalph <raulmoretamartin@gmail.com>
2022-12-18 19:09:13 +0100

    Cambio de nombres en las rutas del sidebar.

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -171,7 +171,7 @@ export default function Header() {
                         <svg style={{ margin: '20px 26.7px 20px 48px' }} xmlns="http://www.w3.org/2000/svg" width="24" height="27" fill="currentColor" class="bi bi-grid-1x2" viewBox="0 0 16 16">
                             <path d="M6 1H1v14h5V1zm9 0h-5v5h5V1zm0 9v5h-5v-5h5zM0 1a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm9 0a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V1zm1 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1h-5z" />
                         </svg>
-                        <SubTitle>Guest</SubTitle>
+                        <SubTitle>Users</SubTitle>
                     </SideLinks>
 
                     <SideLinks url="/contact">
@@ -179,7 +179,7 @@ export default function Header() {
                         <svg style={{ margin: '20px 26.7px 20px 48px' }} xmlns="http://www.w3.org/2000/svg" width="24" height="27" fill="currentColor" class="bi bi-grid-1x2" viewBox="0 0 16 16">
                             <path d="M6 1H1v14h5V1zm9 0h-5v5h5V1zm0 9v5h-5v-5h5zM0 1a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V1zm9 0a1 1 0 0 1 1-1h5a1 1 0 0 1 1 1v5a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1V1zm1 8a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-5a1 1 0 0 0-1-1h-5z" />
                         </svg>
-                        <SubTitle>Concierge</SubTitle>
+                        <SubTitle>Contact</SubTitle>
                     </SideLinks>
 
 
